fee324379a2adf13b39320c1f54eaddc27340403
TK1998GBPUAT <65799675+TK1998GBPUAT@users.noreply.github.com>
2020-12-27 09:20:19 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+l=[]
+for i in range(2000,3201):
+    if(i%7==0 and i%5!=0):
+        l.append(str(i))
+print(','.join(l))        
+
+
+# In[ ]:
+
+
+name=input('Enter you name:')
+l=name.split()
+print(l[1]+' '+l[0])
+
+
+# In[16]:
+
+
+pi=22/7
+r=int(input('Enter radius'))
+v=4/3*pi*r**2
+v
+
+
+# In[7]:
+
+
+
+
+
+# In[8]:
+
+
+
+
+
+# In[10]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
